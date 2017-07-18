763d3efde3d758e44ac2fe9f95f78b95d6dbcbd1
Patrick Oyarzun <patrickoyarzun@gmail.com>
2017-07-17 20:40:55 -0400

    Add i3status config

--- install.sh
+++ install.sh
@@ -20,6 +20,7 @@ link ./.bash_aliases ~/.bash_aliases
 link ./.profile ~/.profile
 
 link ./i3 ~/.config/i3
+link ./i3status ~/.config/i3status
 link ./compton.conf ~/.config/compton.conf
 
 link ./.gitconfig ~/.gitconfig
