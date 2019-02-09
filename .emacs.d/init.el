79eea5bba187f19d5c5b5617d64a35e5f95267b8
Patrick Oyarzun <patrickoyarzun@gmail.com>
2019-02-09 15:10:56 -0600

    Simplify MELPA config

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,22 +1,9 @@
 ;; Config from: https://melpa.org/#/getting-started
 
 (require 'package)
-(let* ((no-ssl (and (memq system-type '(windows-nt ms-dos))
-                    (not (gnutls-available-p))))
-       (proto (if no-ssl "http" "https")))
-  (when no-ssl
-    (warn "\
-Your version of Emacs does not support SSL connections,
-which is unsafe because it allows man-in-the-middle attacks.
-There are two things you can do about this warning:
-1. Install an Emacs version that does support SSL and be safe.
-2. Remove this warning from your init file so you won't see it again."))
-  ;; Comment/uncomment these two lines to enable/disable MELPA and MELPA Stable as desired
-  (add-to-list 'package-archives (cons "melpa" (concat proto "://melpa.org/packages/")) t)
-  ;;(add-to-list 'package-archives (cons "melpa-stable" (concat proto "://stable.melpa.org/packages/")) t)
-  (when (< emacs-major-version 24)
-    ;; For important compatibility libraries like cl-lib
-    (add-to-list 'package-archives (cons "gnu" (concat proto "://elpa.gnu.org/packages/")))))
+
+(add-to-list 'package-archives (cons "melpa" "https://melpa.org/packages/") t)
+
 (package-initialize)
 
 ;; Customize
@@ -33,6 +20,7 @@ There are two things you can do about this warning:
    (quote
     (parinfer parinfer-mode move-text flx counsel ido-vertical-mode smex flx-ido expand-region multiple-cursors company-terraform terraform-mode company swiper wgrep ag projectile magit use-package)))
  '(swiper-goto-start-of-match t))
+
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -110,7 +98,7 @@ There are two things you can do about this warning:
   (magit-define-popup-action 'magit-commit-popup ?i "Commit using ci" 'magit-ci-create ?c t)
 
   (defun magit-ci-create (&optional args)
-    "Create a new commit on `HEAD' using `ci'.
+    "Create a new commit on `HEAD' using `ci'.))
 With a prefix argument, amend to the commit at `HEAD' instead.
 \n(git commit [--amend] ARGS)"
     (interactive
