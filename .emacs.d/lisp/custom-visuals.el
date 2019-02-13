bf5de2968f7f405aa1196af52e8623a41003d120
Patrick Oyarzun <patrickoyarzun@gmail.com>
2019-02-12 22:52:56 -0600

    Add shell panel

--- .emacs.d/lisp/custom-visuals.el
+++ .emacs.d/lisp/custom-visuals.el
@@ -32,3 +32,35 @@
   (setq neo-theme (if (display-graphic-p) 'icons 'arrow)))
 
 
+(add-to-list 'display-buffer-alist
+     '("*eshell-panel" (display-buffer-in-side-window) (side . bottom)))
+(add-to-list 'display-buffer-alist
+     '("*term-panel" (display-buffer-in-side-window) (side . bottom)))
+
+
+(defun oyarzun/eshell-panel ()
+  "Emulate intellij panel hide/show for eshell"
+  (interactive)
+  (if (ignore-errors eshell-panel-buffer)
+      (if (eq (selected-window) (get-buffer-window eshell-panel-buffer))
+          (delete-window)
+          (pop-to-buffer eshell-panel-buffer))
+      (setq eshell-panel-buffer
+        (let ((eshell-buffer-name "*eshell-panel*"))
+          (eshell)))))
+
+(defun oyarzun/term-panel ()
+  "Emulate intellij panel hide/show for eshell"
+  (interactive)
+  (if (ignore-errors term-panel-buffer)
+      (if (eq (selected-window) (get-buffer-window term-panel-buffer))
+          (delete-window)
+          (pop-to-buffer term-panel-buffer))
+      (setq term-panel-buffer
+        (ansi-term "/bin/bash" "*term-panel*"))))
+
+
+(bind-key "M-2" 'oyarzun/eshell-panel)
+(bind-key "M-3" 'oyarzun/term-panel)
+
+
