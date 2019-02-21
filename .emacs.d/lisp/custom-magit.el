8a8e80da705e2227ebf5b30bb7a25d661c8d0643
Patrick Oyarzun <patrickoyarzun@gmail.com>
2019-02-21 15:28:27 -0600

    Fix git ci behavior in new version of magit

--- .emacs.d/lisp/custom-magit.el
+++ .emacs.d/lisp/custom-magit.el
@@ -1,20 +1,14 @@
 (use-package magit
   :bind (("C-x g" . 'magit-status))
   :config
-  ;; Add git ci support (from https://github.com/jvshahid/emacs-config/blob/7d777f5a6d8da3af119ac1228b0b6cf34ba33aba/emacs.d/lisp/conf-magit.el#L10-L28)
+  
+  (defun shahid/magit-replace-command (args)
+    (message "called with: %S " args)
+    (pcase-let ((`(,cmd . ,args) args))
+      (cond
+       ((string-equal cmd "commit") (cons "ci" args))
+       (t (cons cmd args)))))
 
-  (magit-define-popup-action 'magit-commit-popup ?i "Commit using ci" 'magit-ci-create ?c t)
-
-  (defun magit-ci-create (&optional args)
-    "Create a new commit on `HEAD' using `ci'.)
-With a prefix argument, amend to the commit at `HEAD' instead.
-\n(git commit [--amend] ARGS)"
-    (interactive
-     (if current-prefix-arg
-       (list (cons "--amend" (magit-commit-arguments)))
-       (list (magit-commit-arguments))))
-    (when (member "--all" args)
-      (setq this-command 'magit-commit-all))
-    (when (setq args (magit-commit-assert args))
-      (let ((default-directory (magit-toplevel)))
-        (magit-run-git-with-editor "ci" args)))))
+  (advice-add #'magit-run-git-with-editor
+              :filter-args
+              #'shahid/magit-replace-command))
